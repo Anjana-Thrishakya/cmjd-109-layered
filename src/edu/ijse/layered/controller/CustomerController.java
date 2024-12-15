1f8076c7c2e6aa131ce2baa2ae83e8823e25d2d3
Anjana Thrishakya <anjanathrishakya@gmail.com>
2024-12-15 12:46:58 +0530

    Controllers

---
+++ src/edu/ijse/layered/controller/CustomerController.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.layered.controller;
+
+import edu.ijse.layered.dto.CustomerDto;
+
+
+/**
+ *
+ * @author anjan
+ */
+public class CustomerController {
+    
+    
+    public CustomerDto searchCustomer(String id) throws Exception{
+        return null;
+    }
+}
---
+++ src/edu/ijse/layered/controller/ItemController.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.layered.controller;
+
+import edu.ijse.layered.dto.ItemDto;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author anjan
+ */
+public class ItemController {
+    
+    public String saveItem(ItemDto itemDto) throws Exception{
+        return null;
+    }
+    
+    public String updateItem(ItemDto itemDto) throws Exception{
+        return null;
+    }
+    
+    public ArrayList<ItemDto> getAll() throws Exception{
+        return null;
+    }
+    
+    public ItemDto searchItem(String itemCode) throws Exception{
+        return null;
+    }
+    
+}
